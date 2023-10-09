07f906f077f4873c7c6770f5829af64925bb945d
LoneDev6 <leoxy98@gmail.com>
2023-10-09 09:37:28 +0200

    Fixed Velocity

--- src/main/java/dev/lone/bungeepackfix/velocity/Main.java
+++ src/main/java/dev/lone/bungeepackfix/velocity/Main.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 @Plugin(
         id = "bungeepackfix",
         name = "BungeePackFix (Velocity)",
-        version = "1.1.3",
+        version = "1.1.4",
         description = "Avoid sending resourcepacks again if it's the same resourcepack. Useful when you switch servers.", authors = {"LoneDev", "YoSoyVillaa"}
 )
 public class Main
